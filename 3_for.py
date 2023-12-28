67f75ebe35b82aba799b03700aad0924d8a64f4e
Savichiha <151385864+Savichiha@users.noreply.github.com>
2023-12-28 19:03:04 +0300

    Update 3_for.py
    исправлено

--- 3_for.py
+++ 3_for.py
@@ -17,10 +17,6 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
 stock = [
     {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
@@ -42,11 +38,9 @@ for all_phones in stock:
     summ_sold =sum(phone['items_sold'])
     all_ph += summ_sold
 print(f'Всего продано ед.: {all_ph}')
-
-for sold_avg in stock:
-    phone_name = sold_avg["product"]
-    sold_avg_phone = sum(sold_avg['items_sold']) / len(sold_avg['items_sold'])
-print(f'Средее количество продаж: {sold_avg_phone}')
+print(f'Среднее кол-во продаж всех телефонов в ед: {summ_sold / avg_sold_phone}')
+    pass
     
 if __name__ == "__main__":
     main()
+

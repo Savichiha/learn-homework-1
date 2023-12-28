3f72a917cb43ccbff335b1b43e5fa4b4d3ae4c33
Savichiha <151385864+Savichiha@users.noreply.github.com>
2023-12-28 19:04:02 +0300

    Update 7_exception2.py
    исправлено

--- 7_exception2.py
+++ 7_exception2.py
@@ -12,16 +12,12 @@
   ValueError и TypeError, если приведение типов не сработало.
     
 """
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
 def discounted(price, discount, max_discount=20):
     try:
 
         price = float(price)
         discount = float(discount)
-        max_discount = abs(max_discount)
+        max_discount = int(max_discount)
         if max_discount >= 100:
             raise ValueError('Слишком большая максимальная скидка')
         if discount >= max_discount:
